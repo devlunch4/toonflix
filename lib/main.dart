113d20d989bcd1f267b2d8cadae32400e0d7d73f
devlunch In MacOs <devlunch4@gmail.com>
2023-06-16 19:08:44 -0400

    #4.4 Widget Lifecycle (07:18)

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,13 +30,17 @@ class _AppState extends State<App> {
           ),
         ),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDD8),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDD8),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? const MyLargeTitle() : const Text("Nothing!"),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
@@ -37,13 +49,31 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print("InitState!");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("Dispose!");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("Build!");
     return Text(
       "My Large Title",
       style: TextStyle(
