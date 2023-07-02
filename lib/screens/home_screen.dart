5e4f03656864b2be706acc91ff2c92f311c9405e
devlunch In MacOs <devlunch4@gmail.com>
2023-07-02 16:54:42 -0400

    #6.1 AppBar (05:58)

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Today's Toon's (오늘의 웹툰)",
+          style: TextStyle(
+            fontSize: 26,
+            // fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
