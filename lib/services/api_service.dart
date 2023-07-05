56eb324f57a998fc7fe65e77d58d5e1f2ea44ff4
devlunch In MacOs <devlunch4@gmail.com>
2023-07-04 20:15:17 -0400

    #6.4 Recap (06:09)

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -13,9 +13,10 @@ class ApiService {
     final response = await http.get(url);
     //async programming - wait something do happened. side effect
     if (response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(response.body);
+      final webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
