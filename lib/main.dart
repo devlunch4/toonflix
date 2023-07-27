dc63483a9fce6925af2d459d6a735d2565fdd916
devlunch In MacOs <devlunch4@gmail.com>
2023-07-27 01:31:23 -0400

    #6.8 Webtoon Card (13:55)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
+// error 403 solve #1
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides(); // error 403 solve #2
+
   runApp(const App());
 }
 
